909e150924eb843ad6f4884d3b65f672f1ac83df
ocavue <ocavue@gmail.com>
2024-10-08 14:19:57 +1100

    remove dead code

--- ec.config.mjs
+++ ec.config.mjs
@@ -3,9 +3,7 @@ import { defineEcConfig } from 'astro-expressive-code'
 // https://expressive-code.com/reference/configuration/
 export default defineEcConfig({
   themeCssRoot: 'html',
-  // themes: {light: "github-light", dark: 'github-dark'},
   themeCssSelector: (theme) => {
-    console.log('theme.name', theme.name)
     if (theme.name.includes('dark')) {
       return '.dark'
     }
