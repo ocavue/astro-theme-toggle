b25f0cb73f3486ed425ee10610194338c51adb3e
ocavue <ocavue@gmail.com>
2026-09-04 14:12:17 +1000

    fix

--- src/react/theme-toggle.ts
+++ src/react/theme-toggle.ts
@@ -1,12 +1,25 @@
 import * as React from 'react'
 
+import { loadBuildinAnimation, type AnimationName } from '../animations/index.ts'
+import type { TransitionAnimation } from '../animations/types.ts'
 import { registerCustomElement } from '../custom-element.ts'
 
 
-type ThemeToggleProps = React.HTMLAttributes<HTMLElement>
+type ThemeToggleProps = React.HTMLAttributes<HTMLElement> & {
+  animation ?:AnimationName |  TransitionAnimation
+}
 
 const ThemeToggle: React.FC = (props: ThemeToggleProps) => {
   registerCustomElement()
+
+  const animationName = typeof props.animation === 'string' ? props.animation : undefined
+
+  React.useEffect(() => {
+    if (animationName){
+    void   loadBuildinAnimation(animationName)
+    }
+
+  }, [])
 
   return React.createElement('transition-theme-toggle', {
     tableIndex: 0,
