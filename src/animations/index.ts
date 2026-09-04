6abc72c7015eabbae8c7e8f8a9ad820394cddaf3
ocavue <ocavue@gmail.com>
2026-09-04 14:05:16 +1000

    fix

--- src/animations/index.ts
+++ src/animations/index.ts
@@ -6,7 +6,7 @@ type AnimationName = 'circle'
 async function loadAnimation(name: AnimationName): Promise<TransitionAnimation> {
   switch (name) {
     case 'circle':
-      return import('./circle.ts').then((module) => module.default)
+      return await import('./circle.ts').then((module) => module.default)
 
     default:
       throw new Error(`Animation "${name}" not found`)
